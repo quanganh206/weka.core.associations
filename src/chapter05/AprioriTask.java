ce2652c4d4dc685e11dbbf164c6882fa9d74958d
Quang Anh <quanganh@aiti.com.vn>
2017-12-04 16:16:11 +0700

    update time processing

--- src/chapter05/AprioriTask.java
+++ src/chapter05/AprioriTask.java
@@ -3,15 +3,21 @@ package chapter05;
 import weka.core.Instances;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.time.Duration;
+import java.time.Instant;
 
 import weka.associations.Apriori;
 
 public class AprioriTask {
 	public static void main(String args[]) throws Exception {
+		Instant start = Instant.now();
 		Instances data = new Instances(new BufferedReader(new FileReader("data/supermarket.arff")));
 		
 		Apriori model = new Apriori();
 		model.buildAssociations(data);
 		System.out.println(model);
+		
+		Instant end = Instant.now();
+		System.out.println("Processing Time: " + Duration.between(start, end));
 	}
 }
--- src/chapter05/FPGrowthTask.java
+++ src/chapter05/FPGrowthTask.java
@@ -3,15 +3,23 @@ package chapter05;
 import weka.core.Instances;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.time.Duration;
+import java.time.Instant;
+
 import weka.associations.FPGrowth;
 
 public class FPGrowthTask {
 	public static void main(String args[]) throws Exception {
+		Instant start = Instant.now();
+		
 		Instances data = new Instances(new BufferedReader(new FileReader("data/supermarket.arff")));
 		
 		FPGrowth model = new FPGrowth();
 		model.buildAssociations(data);
 		
 		System.out.println(model);
+		
+		Instant end = Instant.now();
+		System.out.println("Processing Time: " + Duration.between(start, end));
 	}
 }
